6f949763f23e9c84f753ed64efd52e2e499d962f
LarissaAbr2801 <abramova.la.28@gmail.com>
2022-06-19 23:28:08 +0300

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String text = "аббб енвотао пвоотф фывв абв";
+        Function<String, List<String>> function = generateDictionary(text);
+        System.out.println(function.apply(text));
+    }
+
+    public static Function<String, List<String>> generateDictionary(String text) {
+        return x -> Arrays.stream(text.split(" "))
+                .sorted()
+                .collect(Collectors.toList());
+    }
+}
